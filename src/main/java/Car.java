12efe0c5073181b1327a0fc51afcb44e0cd094e7
Данислав Тодоров <DATodorov19@codingburgas.bg>
2022-10-19 12:49:47 +0300

    Initial commit

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,3 +1,9 @@
 public class Car {
-
+    String brand;
+    String model;
+    void start(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+        System.out.println(brand + " " + model);
+    }
 }
